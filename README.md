253bd54b9835d5c409ebfe05d08d2b1a4905661b
Jacob Habib <habib.50@osu.edu>
2024-05-17 00:22:54 -0500

    init

---
+++ README.md
@@ -0,0 +1,8 @@
+## WIP
+
+As of 5/17/24 I'm working on getting content-checker to support both ES Modules and CommonJS. This library is tracking progress
+towards that goal and eventually the repo will work by running: 
+
+```
+node index.js
+```
---
+++ index.js
@@ -0,0 +1,68 @@
+const { Filter } = require('content-checker');
+const dotenv = require('dotenv');
+
+// Load environment variables from .env file
+dotenv.config();
+
+const apiKey = process.env.OPEN_MODERATOR_API_KEY;
+
+if (!apiKey) {
+    console.error('API key not found. Please add it to the .env file.');
+}
+
+// Standard Text Moderation
+const filter = new Filter();
+console.log(filter.clean("Don't be an ash0le")); // Don't be an ******
+
+// Placeholder Overrides
+const customFilter = new Filter({ placeHolder: 'x' });
+console.log(customFilter.clean("Don't be an ash0le")); // Don't be an xxxxxx
+
+// Regex Overrides
+const regexFilter = new Filter({ replaceRegex: /[A-Za-z0-9가-힣_]/g });
+console.log(regexFilter.clean("Don't be an ash0le")); // Don't be an ******
+
+// Add words to the blacklist
+filter.addWords('some', 'bad', 'word');
+console.log(filter.clean('some bad word!')); // **** *** ****!
+
+// Instantiate with an empty list
+const emptyListFilter = new Filter({ emptyList: true });
+console.log(emptyListFilter.clean('hell this wont clean anything')); // hell this wont clean anything
+
+// Remove words from the blacklist
+filter.removeWords('some', 'bad', 'word');
+console.log(filter.clean('some bad word!')); // some bad word!
+
+// AI Text Moderation
+const aiFilter = new Filter({ openModeratorAPIKey: apiKey });
+aiFilter.isProfaneAI('your string here').then(response => {
+    if (response.profane) {
+        console.log('Profanity found. Types: ', response.type.join(', '));
+    } else {
+        console.log('No profanity found');
+    }
+});
+
+// AI Image Moderation
+const imageInput = document.createElement('input');
+imageInput.type = 'file';
+imageInput.accept = 'image/png, image/jpeg';
+
+imageInput.addEventListener('change', async (event) => {
+    const file = event.target.files[0];
+    if (file) {
+        try {
+            const response = await aiFilter.isImageNSFW(file);
+            if (response.nsfw) {
+                console.log('NSFW content detected. Types:', response.type.join(', '));
+            } else {
+                console.log('Image is safe.');
+            }
+        } catch (error) {
+            console.error('Error checking image:', error);
+        }
+    }
+});
+
+document.body.appendChild(imageInput);
